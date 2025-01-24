a2d3b6f17e8e534e4c66bcf4b700ec716312a7d5
Jhonatan <ojedasnachezjhonatan@gmail.com>
2025-01-24 10:14:26 -0500

    configuracion inicial

---
+++ src/main/java/com/isai/localsapirestfull/LocalsApiRestfullApplication.java
@@ -0,0 +1,13 @@
+package com.isai.localsapirestfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalsApiRestfullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalsApiRestfullApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isai/localsapirestfull/LocalsApiRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.localsapirestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalsApiRestfullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
